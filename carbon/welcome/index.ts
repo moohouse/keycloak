03a265f5544bd95b098e69941827a3b35a96fb0c
moo <moo@moo.is>
2024-03-06 15:36:37 +0900

    Bump

--- carbon/welcome/index.ts
+++
@@ -1,4 +0,0 @@
-document.addEventListener('load', () => {
-	const redirectButton = document.getElementById('redirect-button');
-	redirectButton.click();
-});
--- carbon/welcome/resources/js/bundle.js
+++
@@ -1,4 +0,0 @@
-/*! F4ERP Copyright 2024 */(()=>{var __webpack_modules__={"./src/welcome/index.ts":
-/*!******************************!*\
-  !*** ./src/welcome/index.ts ***!
-  \******************************/()=>{eval("document.addEventListener('load', () => {\n\tconst redirectButton = document.getElementById('redirect-button');\n\tredirectButton.click();\n});\n\n\n//# sourceURL=webpack://keycloak-carbon-theme/./src/welcome/index.ts?")}},__webpack_exports__={};__webpack_modules__["./src/welcome/index.ts"]()})();
